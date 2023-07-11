321784e1dd62ebd606a20872a78d4abc55ae84b0
chihirooooooooo <casanoma@uark.edu>
2023-07-11 11:48:26 -0500

    completed

---
+++ slot-machine/slot-machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot-machine
+//
+//  Created by Chihiro Asanoma on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
